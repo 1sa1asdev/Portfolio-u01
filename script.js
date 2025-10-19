9ec4fc7ff38fba4772b80f858cc1b61c43e7d97b
izuvell1 <isaiasprio1@gmail.com>
2025-10-19 23:51:22 +0200

    För netlify

---
+++ script.js
@@ -0,0 +1,21 @@
+const hamburgerButton = document.querySelector('.hamburger-menu');
+const navMenu = document.querySelector('.navlinks');
+const navLinks = document.querySelectorAll('.navlinks a');
+
+hamburgerButton.addEventListener('click', () => {
+  const isOpened = hamburgerButton.getAttribute('aria-expanded') === 'true';
+  hamburgerButton.setAttribute('aria-expanded', !isOpened);
+  
+  navMenu.classList.toggle('nav-open');
+  document.body.classList.toggle('no-scroll');
+});
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    if (navMenu.classList.contains('nav-open')) {
+      hamburgerButton.setAttribute('aria-expanded', 'false');
+      navMenu.classList.remove('nav-open');
+      document.body.classList.remove('no-scroll');
+    }
+  });
+});
